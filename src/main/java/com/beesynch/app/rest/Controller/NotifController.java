d26c2771e9df7bffb9e4d5ded99a1a31845a01f3
Jassy Navarro <jascentpearlnavarro@gmail.com>
2025-01-24 12:45:00 +0800

    HIVEMATE'S TASK LIST

--- src/main/java/com/beesynch/app/rest/Controller/NotifController.java
+++ src/main/java/com/beesynch/app/rest/Controller/NotifController.java
@@ -1,8 +1,10 @@
 package com.beesynch.app.rest.Controller;
 
+import com.beesynch.app.rest.Repo.NotificationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/notifications")
@@ -10,4 +12,5 @@ public class NotifController {
 
     //add get notif by id
     //add get notif by hive id
+
 }
--- src/main/java/com/beesynch/app/rest/DTO/MembersTaskListDTO.java
+++ src/main/java/com/beesynch/app/rest/DTO/MembersTaskListDTO.java
@@ -3,19 +3,29 @@ package com.beesynch.app.rest.DTO;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({
-        "img_path", "firstName"
+        "userId", "img_path", "firstName",
 })
 public class MembersTaskListDTO {
+    private Long userId;
     private String firstName;
     private String img_path;
 
+
     // Constructor that matches the query
-    public MembersTaskListDTO(String firstName, String img_path) {
+    public MembersTaskListDTO(Long userId, String firstName, String img_path) {
+        this.userId = userId;
         this.firstName = firstName;
         this.img_path = img_path;
     }
 
     // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     public String getFirstname() {
         return firstName;
     }
