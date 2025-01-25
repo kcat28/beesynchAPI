049be15900f978e0980048c2490038fd08de29c4
John Efren Gannaban <jefjef1412@gmail.com>
2025-01-25 21:50:40 +0800

    added notification controllers

--- src/main/java/com/beesynch/app/rest/Controller/NotifController.java
+++ src/main/java/com/beesynch/app/rest/Controller/NotifController.java
@@ -1,16 +1,36 @@
 package com.beesynch.app.rest.Controller;
 
+import com.beesynch.app.rest.Models.Notification;
 import com.beesynch.app.rest.Repo.NotificationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/notifications")
 public class NotifController {
 
-    //add get notif by id
-    //add get notif by hive id
+    @Autowired
+    private NotificationRepo notificationRepo;
+
+    //get notif by notif id
+    @GetMapping("/getbyId/{notifId}")
+    public Optional<Notification> getNotificationById(@PathVariable("notifId") Long notifId){
+        return notificationRepo.findById(notifId);
+    }
+
+    // get notif by user id
+    @GetMapping("/getbyUser/{user_id}")
+    public List<Notification> user_id(@PathVariable("user_id") Long user_id){
+        return notificationRepo.findByUser_id(user_id);
+    }
+
+    //get notif by Hive id (those with matching users only)
+    @GetMapping("/getbyHive/{hive_id}")
+    public List<Notification> hive_id(@PathVariable("hive_id") Long hive_id){
+        return notificationRepo.findbyHive(hive_id);
+    }
 
 }
--- src/main/java/com/beesynch/app/rest/Repo/NotificationRepo.java
+++ src/main/java/com/beesynch/app/rest/Repo/NotificationRepo.java
@@ -1,6 +1,18 @@
 package com.beesynch.app.rest.Repo;
 
 import com.beesynch.app.rest.Models.Notification;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface NotificationRepo extends JpaRepository<Notification, Long>{
+
+    @Query("SELECT n FROM Notification n WHERE n.user.id = :user_id")
+    List<Notification> findByUser_id(@Param("user_id") Long user_id);
+
+    @Query("SELECT n FROM Notification n WHERE n.user.id IN (SELECT hm.user.id FROM HiveMembers hm WHERE hm.hive.hive_id = :hive_id)")
+    List<Notification> findbyHive(@Param("hive_id") Long hive_id);
 }
