6c0a229f6bfefa1713f9e493097e79f365c09b09
ashikaede <colocadojd@gmail.com>
2025-02-26 01:04:24 +0800

    removed duplicated code

--- src/main/java/com/beesynch/app/rest/Controller/AuthController.java
+++ src/main/java/com/beesynch/app/rest/Controller/AuthController.java
@@ -47,16 +47,9 @@ public class AuthController {
             User user = userRepo.findByUserName(userDTO.getUser_name()); // Find user by username
 
             // Validate the retrieved user and its password
-
             if (user == null || !passwordEncoder.matches(userDTO.getUser_password(), user.getUser_password())) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Username or Password");
             }
-
-             if (user == null || !passwordEncoder.matches(userDTO.getUser_password(),
-             user.getUser_password())) {
-             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Username or Password");
-             }
-
 
             // Step 2: Generate JWT token for authenticated user
             String token = jwtUtil.generateToken(user.getId()); // Use user ID instead of username
@@ -69,7 +62,6 @@ public class AuthController {
                     "firstName", user.getFirst_name(),
                     "lastName", user.getLast_name(),
                     "email", user.getUser_email()));
-
 
         } catch (Exception e) {
             // Log the exception (use a logger in production)
